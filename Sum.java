85fb32a94f1f3337462e057ff97deb47f5393301
Sayem2004 <mdalimransayem@gmail.com>
2025-04-09 22:23:10 +0600

    Lab Work

---
+++ Sum.java
@@ -0,0 +1,15 @@
+public class Sum
+{
+	public static void main(String[]args)
+	{
+		int a=50,b=111;
+		int sum=a+b;
+		int sub=a-b;
+		int mul=a*b;
+		float div=(float)a/b;
+		System.out.println("Summatino is = "+sum);
+		System.out.println("Subtration is = "+sub);
+		System.out.println("Multiply is = "+mul);
+	    System.out.println("Divide is = "+div);
+	}
+}
