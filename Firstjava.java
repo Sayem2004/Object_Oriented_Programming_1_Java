2d98fe2d66401d1b93873c189f581509665c7426
Sayem2004 <mdalimransayem@gmail.com>
2025-04-09 22:17:21 +0600

    Practices

---
+++ Firstjava.java
@@ -0,0 +1,18 @@
+public class Firstjava
+{
+	public static void main(String[]args)
+	{
+		System.out.println("&&&&&&&&&&&&&&&&&&&&&&");
+		System.out.println(" ");
+		System.out.print("\n");
+		System.out.println("Good \n\nMorning EveryOne.");
+		System.out.println(" ");
+		System.out.println("How are YOU?");
+		System.out.println(" ");
+		System.out.println("What are you doing ?");
+		System.out.println(" ");
+		System.out.println(" ");
+		System.out.println("&&&&&&&&&&&&&&&&&&&&& ?");
+		
+	}
+}
---
+++ myinfo.java
@@ -0,0 +1,12 @@
+public class myinfo
+{
+    public static void main(String[]args)
+	{
+	    System.out.println("--------------------");
+		System.out.println(" Md.Al-Imran Sayem  ");
+		System.out.println(" CSe              ");
+		System.out.println(" Barguna          ");
+		System.out.println("--------------------");
+		
+    }
+}
