e476728d79e3c158d6480afbd4252ab6bec511eb
Sayem2004 <mdalimransayem@gmail.com>
2025-04-09 22:21:16 +0600

    Lab Work

---
+++ swap.java
@@ -0,0 +1,13 @@
+public class swap
+{
+	public static void main(String[] args)
+	{
+       int a=7;
+       int b=9,temp;
+   temp=a;
+a=b;
+b=temp;
+  System.out.println("B="+b);
+  System.out.println("A="+a);
+	}
+}	
---
+++ vowel.java
@@ -0,0 +1,49 @@
+public class vowel
+{
+	public static void main(String[] args)
+	{
+		char c='c';
+		switch(c)
+		{
+			
+			case 'a':
+			System.out.println("Its a vowel ");
+			break;
+			
+			case 'e':
+			System.out.println("Its a vowel ");
+			break;
+			case 'i':
+			System.out.println("Its a vowel ");
+			break;
+			case 'o':
+			System.out.println("Its a vowel ");
+			break;
+			case 'u':
+			System.out.println("Its a vowel ");
+			break;
+			case 'A':
+			System.out.println("Its a vowel ");
+			break;
+			case 'E':
+			System.out.println("Its a vowel ");
+			break;
+			case 'I':
+			System.out.println("Its a vowel ");
+			break;
+			case 'O':
+			System.out.println("Its a vowel ");
+			break;
+			case 'U':
+			System.out.println("Its a vowel ");
+			break;
+			default:
+			System.out.println("Its consonant");
+		}
+	}
+}
+			
+			
+		
+			 
+		
