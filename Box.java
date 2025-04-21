3d4e7c70e6e8ea6da13d223dd692ea9856bc24d7
Sayem2004 <mdalimransayem@gmail.com>
2025-04-21 23:20:39 +0600

    Practices

---
+++ Box.java
@@ -0,0 +1,22 @@
+public class Box
+{
+	private double height;
+	private double width;
+	private double length;
+	private double volume;
+	
+		private void setHeight(double h){height = h;}
+		private void getHeight(){return height;}
+		
+		private void setlenght(double l){length=l;}
+		private void setlenght(double l){length= l;}
+		
+	public double showvolume ()
+	{
+		volume =height*length;
+		return volume;
+	}
+	public static void main(String[]args)
+
+
+		
