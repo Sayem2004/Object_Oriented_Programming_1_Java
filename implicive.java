3d283c9ee763d7e143392cd08e0e4f0412067ca5
Sayem2004 <mdalimransayem@gmail.com>
2025-04-22 10:39:33 +0600

    Practices

---
+++ implicive.java
@@ -0,0 +1,13 @@
+public class implicive
+{
+	public static void main(String[]args)
+	{
+		char T='G';
+		int k= 'T';
+		System.out.println("CHAR="+T); 
+		System.out.println("INT="+k);
+	}
+}
+		
+		
+		
---
+++ typecasting2.java
@@ -0,0 +1,24 @@
+public class typecasting2
+{
+	public static void main(String[]args)
+	{
+	double k =89.99;
+	int g=/*(int)*/k;
+		System.out.println("Double="+k);
+        System.out.println("int="+g);
+	}
+	
+}
+/*public class typecasting2 {
+  public static void main(String[] args) {
+    // create double type variable
+    double num = 10.99;
+
+    // convert into int type
+    int data = (int)num;
+	System.out.println("The double value: " + num);
+    System.out.println("The integer value: " + data);
+    
+
+  }
+}*/
