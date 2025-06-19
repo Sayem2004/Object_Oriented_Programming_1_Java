4b92adcaeb79208098785f6862f2e18349bf282b
Sayem2004 <mdalimransayem@gmail.com>
2025-06-19 17:57:50 +0600

    Practices

---
+++ Account.java
@@ -0,0 +1,42 @@
+public class Account {
+	 private String accName;
+	 private String acid;
+	 private int balance;
+
+	Account() {}
+
+	Account(String accName, String acid, int balance) {
+		this.accName = accName;
+		this.acid = acid;
+		this.balance = balance;
+	}
+
+	public void deposit(int amount)  {
+		this.balance = this.balance + amount;
+		// this.balance += amount;
+	}
+
+	public void withdraw(int amount) {
+		this.balance = this.balance - amount;
+		// this.balance -= amount;
+	}
+
+	public void transfer(int amount, Account receiver) {
+		receiver.deposit(amount);
+		this.withdraw(amount);
+	}
+
+	public static void main(String args[]) {
+		Account Leo = new Account("", "420", 100);
+		Account King = new Account("venom", "001", 50);
+
+		Leo.deposit(10);
+		Leo.withdraw(50);
+
+		King.deposit(30);
+		King.withdraw(30);
+
+		Leo.transfer(25,venim);
+
+	}
+}
---
+++ typecasting.java
@@ -0,0 +1,10 @@
+public class typecasting
+{
+	public static void main(String[]args)
+	{
+		int g=20;
+		double k=g;
+		System.out.println("int="+g);
+		System.out.println("Double="+k);
+	}
+}
